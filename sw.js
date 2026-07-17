8237deefe360b91e7ef154220122f5f4e42403bc
mrkustov927-star <mrkustov927@gmail.com>
2026-07-17 23:03:21 +0300

    feat: add interactive guide map prototype

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "kem-family-routes-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./points.js",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./assets/icon.svg",
+  "./assets/illustrations/station.svg",
+  "./assets/illustrations/tower.svg",
+  "./assets/illustrations/officers-house.svg",
+  "./assets/illustrations/slon.svg",
+  "./assets/illustrations/sea-rapid.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match("./index.html"))));
+});
