9ff25a9370edee088a3650a9f78d3b4a108449f7
mrkustov927-star <mrkustov927@gmail.com>
2026-07-19 01:50:01 +0300

    Fix Supabase project URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.KEM_SUPABASE_CONFIG = Object.freeze({
-  url: "https://qvpnwtuckhseityztifv.supabase.co",
+  url: "https://gvpnwtuckhsejtyztifv.supabase.co",
   publishableKey: "sb_publishable_5dqaJdjsGP0xvy1if1dM-g_t3PTY7s3"
 });
